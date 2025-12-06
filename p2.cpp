bb8477cf008e9bc77510faee2144afc93c57efed
RDPavan <rathodPawan8530@gmail.com>
2025-12-06 18:45:35 +0530

    new code...

---
+++ p2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a,b,add,sub,mult,div;
+    cout<<"Enter the no:";
+    cin>>a>>b;
+    add=a+b;
+    sub=a-b;
+    mult=a*b;
+    div=a/b;
+    cout<<"add="<<add<<"sub="<<sub<<"mult="<<mult<<"div="<<div;
+    getch();
+}
