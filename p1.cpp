e240411b031269ad18f745d2cd58609725781af0
RDPavan <rathodPawan8530@gmail.com>
2025-12-06 16:03:37 +0530

    new code...

---
+++ p1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,mult;
+    cout<<"Enter the no:";
+    cin>>a>>b;
+    mult=a*b;
+    cout<<mult;
+    getch();
+}
