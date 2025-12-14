1d57ec9a0ef91d5b59ad2f1d4707bbc92f24dbe3
RDPavan <rathodPawan8530@gmail.com>
2025-12-14 20:48:43 +0530

    new programs

---
+++ p3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+  
+    double r ,area;
+    cout<<"Enter the no:";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"\narea of circle:"<<area;
+    getch();
+}
