4491a56a2819dd359ec13a7165aae8232f179577
RDPavan <rathodPawan8530@gmail.com>
2025-12-18 18:52:06 +0530

    new programs

---
+++ p7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cout<<"Enter the no:";
+    cin>>i;
+    if(i%2==0){
+        cout<<"Even is:";
+    }
+    else if(i%2!=0){
+        cout<<"Its odd no is:";
+    }
+    return 0;
+}
